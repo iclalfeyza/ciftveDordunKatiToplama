ff380fa0ad5f1b1766504f34d3dc1feef0615cb0
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-11-08 00:54:53 +0300

    last

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner numbers = new Scanner(System.in);
+
+        int p, total = 0;
+
+        do {
+            System.out.print("Enter a number: ");
+            p = numbers.nextInt();
+            if (p % 4 == 0) {
+                total += p;
+            }
+        } while (p % 2 == 0);
+
+        System.out.println("TOTAL:" + total);
+    }
 }
